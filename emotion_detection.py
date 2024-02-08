bedd7a80b13d8f9a6139f8380e7749a5abede724
Relentless1997 <153932319+Relentless1997@users.noreply.github.com>
2024-02-08 11:44:42 -0500

    Update emotion_detection.py
    finds dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,10 +7,17 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } } 
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
+    
+    # separates scores
     anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
     disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
     fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
     joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
     sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
-
-    return (anger_score, disgust_score, fear_score, joy_score, sadness_score)
+    
+    # makes dictionary of responses
+    response_dic = formatted_response["emotionPredictions"][0]["emotion"]
+    
+    # sets dominant_emotion to the key with the max value
+    dominant_emotion = max(response_dic, key=response_dic.get)
+    return (anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion)
