5f08adf63ce272e1f8bf7eccf8cee2d0e88b938a
Relentless1997 <153932319+Relentless1997@users.noreply.github.com>
2024-02-08 19:00:47 -0500

    Update server.py
    Added comments

--- server.py
+++ server.py
@@ -5,55 +5,24 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
-    # gets text from interface
-    text_to_analyze = request.args.get("textToAnalyze")
+    text_to_analyze = request.args.get("textToAnalyze")                # gets text from interface
+    response = emotion_detector(text_to_analyze)                       # runs text through emotion_detector and stores in var response    
     
-    # runs text through emotion_detector and stores in var response
-    response = emotion_detector(text_to_analyze)
-    
-    # breaks down the dictionary that is stored in response
-    # and assigns each sentiment score to a variable
-    anger_score = response["anger"]
-    disgust_score = response["disgust"]
+    anger_score = response["anger"]                                    # (1/2) breaks down the dictionary that is stored in response
+    disgust_score = response["disgust"]                                # (2/2) and assigns each sentiment score to a variable
     fear_score = response["fear"]
     joy_score = response["joy"]
     sadness_score = response["sadness"]
     dominant_emotion = response["dominant_emotion"]
-
-    # puts response into customer requested format
-    return ("For the given statement, the system response is anger: " + str(anger_score) + ", disgust: " + str(disgust_score) + ", fear: " + str(fear_score) + ", joy: " + str(joy_score) + ", sadness: " + str(sadness_score) + ". The dominant emotion is " + str(dominant_emotion)+ ".")
-
-@app.route("/")
-def render_index_page():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5500)
-
-from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector
-
-app = Flask("Emotion Detector")
-
-@app.route("/emotionDetector")
-def emo_detector():
-    text_to_analyze = request.args.get("textToAnalyze") # gets text from interface
-    response = emotion_detector(text_to_analyze)        # runs text through emotion_detector and stores
-    anger_score = response["anger"]                     # stores dic response to variable, same below
-    disgust_score = response["disgust"]
-    fear_score = response["fear"]
-    joy_score = response["joy"]
-    sadness_score = response["sadness"]
-    dominant_emotion = response["dominant_emotion"]
-    if anger_score is None:                             # if emotion_detector return None returns text
+    if anger_score is None:                                            # if emotion_detector return None returns text
         return "Invalid text! Please try again!."
-    else:
+    else:                                                              # puts response into customer requested format
         return ("For the given statement, the system response is anger: " + str(anger_score) + ", disgust: " + str(disgust_score) + ", fear: " + str(fear_score) + ", joy: " + str(joy_score) + ", sadness: " + str(sadness_score) + ". The dominant emotion is " + str(dominant_emotion)+ ".")
 
-
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500)
+
